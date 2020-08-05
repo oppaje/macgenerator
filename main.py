34b69d72f0d9c7c6fb6744ad5c1367790d01fbef
oppaje <52138057+oppaje@users.noreply.github.com>
2020-08-05 02:49:30 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,72 @@
+from random import choice
+import traceback
+
+
+string = '0123456789ABCDEF'
+
+def duplicate(path):
+  f = open(path, 'r', errors='ignnore').readlines()
+  old = len(f)
+  new_f = set(f)
+  new = len(new_f)
+  o = open(path, 'w')
+  for line in new_f:
+    o.write(line)
+  print(f"{old - new} duplicate(s) have been removed!")
+  print("\nFinished! Results have been saved in MacAddresses.txt")
+
+
+def mode1():
+  mac = ''.join(choice(string) for _ in range(6))
+  return f"00:1A:79:{mac[0:2]}:{mac[2:4]}:{mac[4:6]}\n"
+
+
+def mode2():
+  list1 = []
+  letters = [char for char in custom]
+  for letter in letters:
+    if letter == '*':
+      letter = letter.replace('*', ''.join(choice(string)))
+    list1.append(letter)
+  return ''.join(list1) + "\n"
+
+
+def generator():
+  file = 'MacAddresses.txt'
+
+  with open(file, 'w') as o:
+    for x in range(number):
+      if mode == '1':
+        o.write(mode1())
+      elif mode == '2':
+        o.write(mode2())
+      print(f"Generated: {x + 1} MAC addresses", end='\r')
+  o.close()
+  print("\nRemoving duplicates...", end='\r')
+  duplicate(file)
+
+
+if __name__ == '__main__':
+  try:
+    print("""
+                             __  __    _    ____    ____                           _             
+                            |  \/  |  / \  / ___|  / ___| ___ _ __   ___ _ __ __ _| |_ ___  _ __ 
+                            | |\/| | / _ \| |     | |  _ / _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|
+                            | |  | |/ ___ \ |___  | |_| |  __/ | | |  __/ | | (_| | || (_) | |   
+                            |_|  |_/_/   \_\____|  \____|\___|_| |_|\___|_|  \__,_|\__\___/|_|  
+                            
+                                            Coded by: oppaje For NeThinGoez.com
+
+                                      """)
+    mode = input("""Select Mode:
+    1. Default: 00:1A:79:**:**:**
+    2. Custom (Example: 00:1A:79:*8:1B:**)
+    > """)
+    if mode == '2':
+      custom = input("Enter custom mode: ")
+    number = int(input("Enter the amount of Macs to generate: "))
+    generator()
+  except:
+    print(traceback.format_exc())
+    print('Something went wrong, please try again. If the same error keeps happening contact oppaje.')
+  input("Press enter to close: ")
